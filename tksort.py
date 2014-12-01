1a574c984b5db67b4860a98fe3da809771abcec1
Mathias Rav <rav@cs.au.dk>
2014-12-01 21:26:07 +0100

    Add --ignore-case flag

--- tksort.py
+++ tksort.py
@@ -20,13 +20,16 @@ TITLE = re.compile(
     |
     (?P<rest>.*)
     ''',
-    re.I | re.X)
+    re.X)
 
 
-def tk_parse(s):
+def tk_parse(s, args):
     pref = 0
     fu = 0
     title = ''
+    if args.ignore_case:
+        s = s.upper()
+
     for mo in TITLE.finditer(s):
         kind = mo.lastgroup
         value = mo.group(kind)
@@ -50,28 +53,28 @@ def tk_parse(s):
         return None
 
 
-def tk_key(s):
-    p = tk_parse(s)
+def tk_key(s, args):
+    p = tk_parse(s, args)
     if p:
         return (0, p, s)
     else:
         return (1, s)
 
 
-def tk_min_key(s):
+def tk_min_key(s, args):
     words = s.split()
     return min(
         ((0, p, s)
-         for p, word in zip((tk_parse(word) for word in words), words)
+         for p, word in zip((tk_parse(word, args) for word in words), words)
          if p),
         default=(1, s))
 
 
-def tk_max_key(s):
+def tk_max_key(s, args):
     words = s.split()
     return max(
         ((0, p, s)
-         for p, word in zip((tk_parse(word) for word in words), words)
+         for p, word in zip((tk_parse(word, args) for word in words), words)
          if p),
         default=(1, s))
 
@@ -86,9 +89,14 @@ def main():
     parser.add_argument(
         '--max', action='store_const', dest='key', const=tk_max_key)
 
+    parser.add_argument(
+        '-i', '--ignore-case', action='store_true')
+
     args = parser.parse_args()
 
-    for line in sorted(sys.stdin.readlines(), key=args.key):
+    key = lambda s: args.key(s, args)
+
+    for line in sorted(sys.stdin.readlines(), key=key):
         sys.stdout.write(line)
 
 
